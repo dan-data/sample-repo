ecb95e477f1fa9613e52eb447aade3aab1a4c6de
Dan Patwardhan <dan.data2019@gmail.com>
2020-12-03 16:27:27 -0500

    removed some code

--- script.py
+++ script.py
@@ -1,26 +1,10 @@
 # Program to generate a random number between 0 and 9
 # importing the random module
-import math
-import random
-import sys
-from os import rename
-
 import requests
 
 
-def rand(start, end):
-    print(random.randint(start, end))
-    # print("\n")
-    # print(sys.version)
-
-
-def test():
-    name = input("Your name? ")
-    print("Hello, ", name)
-
-
-rand(0, 9)
 r = requests.get("https://google.com")
 print(r.status_code)
+print(r.ok)
 
 # test()
